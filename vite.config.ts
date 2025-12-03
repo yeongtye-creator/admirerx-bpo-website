1e21bf4e55184783607d923fd4a937b73f19dc08
yeongtye-creator <yeongtye@gmail.com>
2025-12-03 21:43:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/admirerx-bpo-website/',  plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: false,
